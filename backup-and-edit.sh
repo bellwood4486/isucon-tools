3f93c2160e2c3a5bc1d49994d29e39c9a1878c67
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2021-08-09 17:58:36 +0900

    Create backup-and-edit.sh

---
+++ backup-and-edit.sh
@@ -0,0 +1,28 @@
+###############################
+# How to use
+# 1. Download this file into "~/".
+# 2. Add alias.
+#    $ alias vim='~/backup-and-edit.sh'
+# 3. Edit a file.
+#    $ vim ~/.bashrc
+###############################
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+readonly BACKUP_DIR=~/backup
+readonly EDITOR=vim
+
+if [[ $# -ne 1 ]]; then
+  echo "error: args count must be 1" 1>&2
+  exit 1
+fi
+
+mkdir -p ${BACKUP_DIR}
+
+if [[ -e $1 ]]; then
+  cp $1 ${BACKUP_DIR}/$(basename $1).$(date "+%Y%m%d_%H%M%S")
+fi
+
+$EDITOR $1
