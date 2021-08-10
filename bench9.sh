4856fa4dfadbcedd571503acdb3838c6544ce81e
SUZUKI Yoshiharu <bellwood4486@gmail.com>
2021-08-10 11:35:38 +0900

    feat: Truncate db slow log

--- bench9.sh
+++ bench9.sh
@@ -4,8 +4,10 @@ set -eu
 set -o pipefail
 
 readonly ACCESS_LOG=/var/log/nginx/access.log
+readonly DB_SLOW_LOG=/var/log/mysql/mysql-slow.log
 
 sudo truncate --size 0 "${ACCESS_LOG}"
+sudo truncate --size 0 "${DB_SLOW_LOG}"
 
 cd ~/isucon9-qualify
 sudo systemctl stop payment.service shipment.service
